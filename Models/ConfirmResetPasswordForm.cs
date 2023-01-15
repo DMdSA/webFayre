4eae2f6a5cf991c91235fdfa0aab22c861c22486
Diogo Rebelo <diogolimarebelo@gmail.com>
2023-01-15 15:19:41 +0000

    Update recover password

--- Models/ConfirmResetPasswordForm.cs
+++ Models/ConfirmResetPasswordForm.cs
@@ -1,9 +1,15 @@
-﻿namespace WebFayre.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebFayre.Models
 {
     public class ConfirmResetPasswordForm
     {
         public string CodeBase64Url { get; set; }
+
+        [Display(Name = "Nova Password")]
         public string newPassword { get; set; }
+
+        [Display(Name = "Reescreva a nova password")]
         public string newPasswordConfirmation { get; set; }
     }
 }
